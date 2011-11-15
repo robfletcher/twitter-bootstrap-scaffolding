b2b43d9291e7fbe448fa70dd63453a6d5ed9cc48
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-11-15 13:10:09 +0000

    activate close button on alerts

--- grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
+++ grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
@@ -5,7 +5,7 @@ class AlertsTagLib {
 	static namespace = "bootstrap"
 
 	def alert = { attrs, body ->
-		out << '<div class="alert-message ' << attrs.class.tokenize().join(" ") << '">'
+		out << '<div class="alert-message ' << attrs.class.tokenize().join(" ") << '" data-alert="alert">'
 		out << '<a class="close" href="#">&times;</a>'
 		out << '<p>' << body() << '</p>'
 		out << '</div>'
