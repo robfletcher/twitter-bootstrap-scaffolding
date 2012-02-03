0ec77f4648c9968505238dde624dfd1cb2cc356b
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-02-03 22:30:32 +0000

    dismissible alerts

--- grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
+++ grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
@@ -5,8 +5,8 @@ class AlertsTagLib {
 	static namespace = "bootstrap"
 
 	def alert = { attrs, body ->
-		out << '<div class="alert alert-block ' << attrs.class.tokenize().join(" ") << '" data-alert="alert">'
-		out << '<a class="close">&times;</a>'
+		out << '<div class="alert alert-block ' << attrs.class.tokenize().join(" ") << '">'
+		out << '<a class="close" data-dismiss="alert">&times;</a>'
 		out << '<p>' << body() << '</p>'
 		out << '</div>'
 	}
