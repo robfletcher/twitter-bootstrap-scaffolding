c5cb4341179e29eea5fc4a6fbba93d2184f57446
jagiro <jagiro.mail@gmail.com>
2012-06-23 21:34:24 +0200

    Changed scaffolding inspired on: http://mrpaulwoods.wordpress.com/2011/01/23/a-pattern-to-simplify-grails-controllers/
    Added view paths like constants to improve the maintenance.
    
    I know that the two first lines (package and domain paths) can be
    improved...

--- src/templates/scaffolding/Controller.groovy
+++ src/templates/scaffolding/Controller.groovy
@@ -1,107 +1,103 @@
-<%=packageName ? "package ${packageName}\n\n" : ''%>import org.springframework.dao.DataIntegrityViolationException
+<%=packageName ? "package ${packageName.replace('domain', 'controller')}\n\n" : ''%>
+import <%=packageName ? "${packageName.replace('controller', 'domain')}.${className}\n" : '${className}'%>
+import org.springframework.dao.DataIntegrityViolationException
+import groovy.lang.Closure
 
 class ${className}Controller {
+	private static final String LIST 	= "/${propertyName}/list"
+	private static final String SHOW 	= "/${propertyName}/show"
+	private static final String CREATE	= "/${propertyName}/create"
+	private static final String EDIT 	= "/${propertyName}/edit"
+	
+	static allowedMethods = [create: ['GET', 'POST'], edit: ['GET', 'POST'], delete: 'POST']
 
-    static allowedMethods = [create: ['GET', 'POST'], edit: ['GET', 'POST'], delete: 'POST']
+	def index() {
+		redirect action: 'list', params: params
+	}
 
-    def index() {
-        redirect action: 'list', params: params
-    }
+	def list() {
+		if (!params.max)	params.max = 10
+		render view:LIST, model: [${propertyName}List: ${className}.list(params), ${propertyName}Total: ${className}.count()]
+	}
 
-    def list() {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [${propertyName}List: ${className}.list(params), ${propertyName}Total: ${className}.count()]
-    }
-
-    def create() {
+	def create() {
 		switch (request.method) {
 		case 'GET':
-        	[${propertyName}: new ${className}(params)]
+			render view:CREATE, model:[${propertyName}: new ${className}(params)]
 			break
 		case 'POST':
-	        def ${propertyName} = new ${className}(params)
-	        if (!${propertyName}.save(flush: true)) {
-	            render view: 'create', model: [${propertyName}: ${propertyName}]
-	            return
-	        }
+			def ${propertyName} = new ${className}(params)
+			if (!${propertyName}.save(flush: true)) {
+				render view: CREATE, model: [${propertyName}: ${propertyName}]
+				return
+			}
 
 			flash.message = message(code: 'default.created.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-	        redirect action: 'show', id: ${propertyName}.id
+			redirect action: 'show', id: ${propertyName}.id
 			break
 		}
-    }
+	}
 
-    def show() {
-        def ${propertyName} = ${className}.get(params.id)
-        if (!${propertyName}) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-            redirect action: 'list'
-            return
-        }
-
-        [${propertyName}: ${propertyName}]
-    }
-
-    def edit() {
-		switch (request.method) {
-		case 'GET':
-	        def ${propertyName} = ${className}.get(params.id)
-	        if (!${propertyName}) {
-	            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-	            redirect action: 'list'
-	            return
-	        }
-
-	        [${propertyName}: ${propertyName}]
-			break
-		case 'POST':
-	        def ${propertyName} = ${className}.get(params.id)
-	        if (!${propertyName}) {
-	            flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-	            redirect action: 'list'
-	            return
-	        }
-
-	        if (params.version) {
-	            def version = params.version.toLong()
-	            if (${propertyName}.version > version) {<% def lowerCaseName = grails.util.GrailsNameUtils.getPropertyName(className) %>
-	                ${propertyName}.errors.rejectValue('version', 'default.optimistic.locking.failure',
-	                          [message(code: '${domainClass.propertyName}.label', default: '${className}')] as Object[],
-	                          "Another user has updated this ${className} while you were editing")
-	                render view: 'edit', model: [${propertyName}: ${propertyName}]
-	                return
-	            }
-	        }
-
-	        ${propertyName}.properties = params
-
-	        if (!${propertyName}.save(flush: true)) {
-	            render view: 'edit', model: [${propertyName}: ${propertyName}]
-	            return
-	        }
-
-			flash.message = message(code: 'default.updated.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-	        redirect action: 'show', id: ${propertyName}.id
-			break
+	def show() {
+		withDomain { ${className} ${propertyName} ->
+			render view:SHOW, model: [${propertyName}: ${propertyName}]
 		}
-    }
+	}
 
-    def delete() {
-        def ${propertyName} = ${className}.get(params.id)
-        if (!${propertyName}) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-            redirect action: 'list'
-            return
-        }
+	def edit() {
+		withDomain { ${className} ${propertyName} ->
+			switch (request.method) {
+			case 'GET':
+				render view:EDIT, model: [${propertyName}: ${propertyName}]
+				break
+			case 'POST':
+				if (params.version) {
+					def version = params.version.toLong()
+					if (${propertyName}.version > version) {<% def lowerCaseName = grails.util.GrailsNameUtils.getPropertyName(className) %>
+						${propertyName}.errors.rejectValue('version', 'default.optimistic.locking.failure',
+								  [message(code: '${domainClass.propertyName}.label', default: '${className}')] as Object[],
+								  "Another user has updated this ${className} while you were editing")
+						render view: EDIT, model: [${propertyName}: ${propertyName}]
+						return
+					}
+				}
+	
+				${propertyName}.properties = params
+	
+				if (!${propertyName}.save(flush: true)) {
+					render view: EDIT, model: [${propertyName}: ${propertyName}]
+					return
+				}
+	
+				flash.message = message(code: 'default.updated.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
+				redirect action: 'show', id: ${propertyName}.id
+				break
+			}
+		}
+	}
 
-        try {
-            ${propertyName}.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-            redirect action: 'list'
-        }
-        catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-            redirect action: 'show', id: params.id
-        }
-    }
+	def delete() {
+		withDomain { ${className} ${propertyName} ->
+			try {
+				${propertyName}.delete(flush: true)
+				flash.message = message(code: 'default.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+				redirect action: 'list'
+			}
+			catch (DataIntegrityViolationException e) {
+				flash.message = message(code: 'default.not.deleted.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+				redirect action: 'show', id: params.id
+			}
+		}
+	}
+	
+    private def withDomain(Closure success) {
+	   ${className} instance = ${className}.get(params.id as Long)
+	   if (instance) {
+		   success.call(instance)
+	   }
+	   else {
+		   flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
+		   redirect action: 'list'
+	   }
+   }
 }
