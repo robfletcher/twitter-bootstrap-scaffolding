9f792dc794f9585536f4b7c0f5f5527d9bd574ef
Peter Clarke <pete@clarkez.co.uk>
2015-09-06 10:27:01 +0100

    Update to make it work locally. Added repos and change versions of jodatime and bootstrap to versions that downloaded & worked with the correct hibernate versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,17 +19,23 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenLocal()
         mavenCentral()
-		
+
+        grailsRepo "https://grails.org/plugins"
+        mavenRepo "http://repo.grails.org/grails/repo"
+
 		mavenRepo "http://snapshots.repository.codehaus.org"
 		mavenRepo "http://repository.codehaus.org"
 		mavenRepo "http://download.java.net/maven/2/"
 		mavenRepo "http://repository.jboss.com/maven2/"
 		mavenRepo "http://maven.springframework.org/milestone/"
-		
+
+        mavenRepo "http://repo.grails.org/grails/libs-releases/"
+        mavenRepo "http://m2repo.spockframework.org/ext/"
+        mavenRepo "http://m2repo.spockframework.org/snapshots/"
     }
 
     dependencies {
-		compile "org.jadira.usertype:usertype.jodatime:2.0.1"
+		compile "org.jadira.usertype:usertype.jodatime:1.9"
     }
 
     plugins {
@@ -38,7 +44,7 @@ grails.project.dependency.resolution = {
         compile ":jquery:1.8.3"
         compile ":resources:1.2"
 
-		runtime ":twitter-bootstrap:2.3.1"
+		runtime ":twitter-bootstrap:2.3.2"
 		runtime ":fields:1.3"
 		runtime ":cache-headers:1.1.5"
 		runtime ":cached-resources:1.0"
